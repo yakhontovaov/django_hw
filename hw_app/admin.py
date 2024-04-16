9bcd5c9a548d2112a409a7d6f82d5b402cd0c3fc
yakhontovaov <o.maksaeva@mail.ru>
2024-04-16 22:54:27 +0300

    Update project

--- hw_app/admin.py
+++ hw_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Client, Product, Order
+from django.db.models import F
 
-# Register your models here.
+
+@admin.action(description='Увеличить количество товара на 10 ед.')
+def restock(modeladmin, request, queryset):
+    queryset.update(quantity=F('quantity') + 10)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number', 'address', 'registration_date']
+    search_fields = ['name', 'email', 'phone_number', 'address']
+    list_filter = ['registration_date']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'quantity', 'added_date']
+    search_fields = ['name', 'description']
+    list_filter = ['added_date']
+    actions = [restock]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'total_amount', 'order_date']
+    search_fields = ['client__name']
+    list_filter = ['order_date']
