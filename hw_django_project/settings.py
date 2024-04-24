7af1ec595092207b92bde2a71c7eda2584d6c376
yakhontovaov <o.maksaeva@mail.ru>
2024-04-24 23:06:01 +0300

    Remove files

--- hw_django_project/settings.py
+++ hw_django_project/settings.py
@@ -20,18 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8j6n8@-ia0snw#b(m@s=#$++x&y72kam55qeun=lzo9x1&7300'
-# SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-8j6n8@-ia0snw#b(m@s=#$++x&y72kam55qeun=lzo9x1&7300'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '192.168.0.13',  # ip ноута
+    # '127.0.0.1',
+    # '192.168.0.13',  # ip ноута
     'yakhont.pythonanywhere.com',
 ]
 
@@ -91,17 +91,17 @@ WSGI_APPLICATION = 'hw_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'yakhont$default',
-        # 'USER': 'yakhont',
-        # 'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        # 'HOST': 'yakhont.mysql.pythonanywhere-services.com',
-        # 'OPTIONS': {
-        #     'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
-        #     'charset': 'utf8mb4',
-        # }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yakhont$default',
+        'USER': 'yakhont',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'yakhont.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
